d50accfa34cb1cf6aedfec710211d86bc3bd72fb
Mattias <90244980+MattiasHelgesson@users.noreply.github.com>
2022-10-15 17:40:04 +0200

    Button Problem
    Animation och button vill inte funka. Mathias kollade på det och hittade inte porblemet. Så nu måste jag göra om en stor del av det

--- Assets/Scripts/Tasks/ClickTask Scripts/WindowRepairPanel.cs
+++ Assets/Scripts/Tasks/ClickTask Scripts/WindowRepairPanel.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Threading;
+
 public class WindowRepairPanel : MonoBehaviour
 {
     [SerializeField]
     Task ownerTask;
 
-    bool gameStarted = false;
+    [SerializeField]
+    Button WindowCracked;
+
+
+    public Animator WindowAnimat;
 
     GameObject lastFirstSelectedGameObject;
 
+    bool blackAllAround = true;
+
+    bool gameStarted = false;
+
+    bool changeSize = false;
+
     //ownerTask.SetAsResolved();
     //Invoke("Hide", 1);
+
+    private void Start()
+    {
+        WindowCracked.onClick.AddListener(WindowPressed);
+    }
+
+    void WindowPressed()
+    {
+        Debug.Log("knappskit");
+        if (blackAllAround == true)
+        {
+            WindowAnimat.SetBool("ChangeSize",true);
+            changeSize = true;
+        }
+       //4 if(changeSize == true)
+    }
 
     public void Show()
     {
