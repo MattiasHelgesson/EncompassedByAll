a0e3135fef525e89e4b0bddb39f12ad4b1d7f45d
Mattias <90244980+MattiasHelgesson@users.noreply.github.com>
2022-11-10 09:04:12 +0100

    Window animtaions, Window drag in progres

--- Assets/Scripts/Tasks/ClickTask Scripts/WindowRepairPanel.cs
+++ Assets/Scripts/Tasks/ClickTask Scripts/WindowRepairPanel.cs
@@ -13,6 +13,7 @@ public class WindowRepairPanel : MonoBehaviour
     [SerializeField]
     Button WindowCracked;
 
+    public GameObject movedObject;
 
     public Animator WindowAnimat;
 
@@ -24,23 +25,57 @@ public class WindowRepairPanel : MonoBehaviour
 
     bool changeSize = false;
 
+    bool WindowScale = false;
+
     //ownerTask.SetAsResolved();
     //Invoke("Hide", 1);
 
     private void Start()
     {
-        WindowCracked.onClick.AddListener(WindowPressed);
+        WindowCracked.onClick.AddListener(WindowCrackedScale);
+        WindowCracked.onClick.AddListener(WindowCrackedMove);
+        Debug.Log("Listeners added");
+        WindowDrag();
     }
 
-    void WindowPressed()
+
+    void WindowDrag()
     {
-        Debug.Log("knappskit");
-        if (blackAllAround == true)
+        Vector3 mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
         {
-            WindowAnimat.SetBool("ChangeSize",true);
-            changeSize = true;
+            Collider2D UncrackedWindow = Physics2D.OverlapPoint(mousePosition);
+            if (UncrackedWindow)
+            {
+                movedObject = UncrackedWindow.transform.gameObject;
+            }
         }
-       //4 if(changeSize == true)
+        
+        
+    }
+    void WindowCrackedScale()
+    {
+        Debug.Log("ChangeSize");
+
+        // Debug.Log("knappskit");
+        // if (blackAllAround == true)
+        // {
+        //     WindowAnimat.SetBool("ChangeSize",true);
+        //     changeSize = true;
+        // }
+        //4 if(changeSize == true)
+
+        WindowAnimat.SetBool("ChangeWindButton", true);
+        WindowScale = true;
+    }
+    void WindowCrackedMove()
+    {
+        Debug.Log("Flytta");
+
+        if (WindowScale == true)
+        {
+            WindowAnimat.SetBool("IfChangeWindButtonTrue", true);
+        }
     }
 
     public void Show()
